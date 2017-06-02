a8c61a06a8058f298a9140b4f6b6191c703d35c4
quasimodeaux <gatchey@yahoo.com>
2017-06-02 13:19:30 -0400

    js changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,34 @@
-//IDs:
-//#searchInput
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+
+
+url += '?' + $.param({
+  'api-key': "769f2ad0d52b43bbb9836c132e41d884",
+  'q' : "michael+jackson"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+  console.log(result.response.docs[0]);
+
+
+
+
+
+
+
+
+
+  
+
+}).fail(function(err) {
+  throw err;
+});
+
+
+
+
+
